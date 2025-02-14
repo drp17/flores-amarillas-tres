2b05007ebd6d4e7472a7af59ca323d1ebaf034fe
drp17 <108010694+drp17@users.noreply.github.com>
2025-02-14 03:19:51 -0500

    Update anim.js

--- anim.js
+++ anim.js
@@ -12,19 +12,4 @@ var lyricsData = [
     
 ];
 
-// Animar las letras
-function updateLyrics() {
-  var time = Math.floor(audio.currentTime);
-  var currentLine = lyricsData.find(
-    (line) => time >= line.time && time < line.time + 6
-  );
 
-  if (currentLine) {
-    // Calcula la opacidad basada en el tiempo en la línea actual
-    var fadeInDuration = 0.1; // Duración del efecto de aparición en segundos
-    var opacity = Math.min(1, (time - currentLine.time) / fadeInDuration);
-
-    // Aplica el efecto de aparición
-    lyrics.style.opacity = opacity;
-    lyrics.innerHTML = currentLine.text;
-  } 
