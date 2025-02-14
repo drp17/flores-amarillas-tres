9513e8d6f9be12ae3ac2d6b9c1b2345b6915cfa9
drp17 <108010694+drp17@users.noreply.github.com>
2025-02-14 02:49:54 -0500

    Update anim.js

--- anim.js
+++ anim.js
@@ -4,7 +4,7 @@ var lyrics = document.querySelector("#lyrics");
 
 // Array de objetos que contiene cada línea y su tiempo de aparición en segundos
 var lyricsData = [
-  { text: "At the time", time: 15 },
+  { text: "Well, you done done me in, you bet I felt it", time: 10 },
   { text: "The whisper of birds", time: 18 },
   { text: "Lonely before the sun cried", time: 27 },
   { text: "Fell from the sky", time: 32 },
